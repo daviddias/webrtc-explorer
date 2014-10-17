f9dc087f29cef9eaa1d613471911bf7608c21ef4
David Dias <daviddias.p@gmail.com>
2014-10-17 16:55:59 +0100

    phase I complete

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var eventEmitter2 = require('eventemitter2').EventEmitter2;
 var simplePeer = require('simple-peer');
 var io = require('socket.io-client');
-
+var bows = require('bows'); 
 var fingerTable = require('./modules/fingerTable').createFingerTable();
-var uuid = require('./uuid');
+var uuid = require('./modules/uuid.js');
 
-exports = module.exports = createNode;
+log = bows('webrtc-chord');
 
+exports = module.exports;
+
+//
 // config: {
 //   signalingURL: <IP or Host of webrtc-chord-signaling-server>
-//   namespace: defaults to /
+//   namespace: defaults to /,
+//   logging: defaults to true,
+//   traces: defaults to false // emit events for connect events and finger table changes
 // }
-function createNode(config) {
+// 
+exports.createNode = function (config) {
+  localStorage.debug = config.logging || true;
+
   var ee = new eventEmitter2({
     wildcard: true,
     newListener: false, 
@@ -22,32 +30,83 @@ function createNode(config) {
   var ioClient = io(url);
   var bootstrapPeer;
 
-  ioClient.on('c-connection-established', function () {
-    // create a simple-peer
-    // ask to connect to another peer (which will present to us the successor)
+  ioClient.on('connect',    startBootstrapProcess);
+  ioClient.on('c-response', receivedResponse);
+  ioClient.on('c-request',  railNewPeer);
 
-
-  });  
-
-  ioClient.on('c-connection-response', function () {
-    // start the bootstraping process.
-    // end of (phase I)
-    // ask if my ID is ok 
-    // ask to connect with my successor! :)
-    // once connected to the sucessor, fill in the fingertable
-
-  });
-
-  ioClient.on('c-connection-request', function () {
-    // some other peer looking to connect to us for bootstrap purposes
+  function startBootstrapProcess() {
+    bootstrapPeer = new simplePeer({ initiator: true, trickle: false });
   
+    bootstrapPeer.on('signal', function (data) {        
+      var peerInvite = { signalData: data };
+      ioClient.emit('s-request', peerInvite);
+      log('Sent Simple Peer data to another Peer');
+    });
+    
+    bootstrapPeer.on('message', function (data) {
+      // Take care here of the messages that will enable us to reach yo our sucessor
+      // TODO LOT OF WORK
+    });
+    
+    bootstrapPeer.on('ready', function() {
+      log('Connected to the network... filling finger table');   
+      // END OF PHASE I
 
-  });
+      // ask if my ID is ok 
+      // ask to connect with my successor! :)
+      // once connected to the sucessor, fill in the fingertable
+    });
 
 
+  }
+
+  function receivedResponse (peerInvite) {
+    if (peerInvite.peersAvailable === false) {
+      log('There aren\'t other peers in the network currently');
+      bootstrapPeer = null;
+      // bootstrapPeer.destroy(); // would only destroy if it there was a connection
+      // setTimeout(function () { startBootstrapProcess(); }, 5000);
+      // this way both peers start trying to connect forever, the first shouldn't
+    } else {
+      log('Received signaling data from our RailingPeer');
+      bootstrapPeer.signal(peerInvite.signalData);      
+    }
+  }
+
+  function railNewPeer (peerInvite) {
+    log('Received request to rail new peer on the network');
+    var railer = new simplePeer({ trickle: false });
+
+    railer.on('signal', function (data) {   
+      peerInvite.signalData = data; // peerInvite will also have the id of the node to get back
+      
+      ioClient.emit('s-response', peerInvite);
+      log('Sent Simple Peer data to the peer railing in');
+    });
+
+    railer.on('message', function (data) {
+      // Here we will handle the messages that will rail in this new peer
+      // TODO LOT OF WORK
+    });
+    
+    railer.on('ready', function() {
+      log('Ready to rail in the new peer');   
+    });
+
+    railer.signal(peerInvite.signalData);
+
+
+  }
 
   return ee;
-}
+};
+
+
+
+
+
+
+
 
 function messageInterpreter (simplePeerObj) {
   // attach all the event listners and respective 
--- modules/uuid.js
+++ modules/uuid.js
@@ -17,3 +17,4 @@ exports.gen = function () {
 exports.get = exports.uuid = function () {
   return uuid;
 };
+
